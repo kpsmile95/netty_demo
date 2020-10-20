54228b9eef5c5d6e7c0781f979987b43d2df496d
wangmian <827486080@qq.com>
2020-10-20 15:11:46 +0800

    时间服务器版本二（多次请求服务器，含有TCP粘包/拆包问题，利用LineBasedFrameDecoder+StringDecoder解决）

---
+++ src/main/java/cn/wm/netty/time2/TimeServerHandler.java
@@ -0,0 +1,33 @@
+package cn.wm.netty.time2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+/**
+ * @Author wangmian
+ * @Date 2020/10/19
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter;
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("time server receive order : " + body + " ; the counter is " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+                new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
