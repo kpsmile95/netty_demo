1c2f95ef4bf7fdf1a9f14bc3d6a4d2bb2253921d
wangmian <827486080@qq.com>
2020-10-21 16:29:37 +0800

    http文件服务器

---
+++ src/main/java/cn/wm/netty/httpfile/HttpFileServer.java
@@ -0,0 +1,63 @@
+package cn.wm.netty.httpfile;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @Author wangmian
+ * @Date 2020/10/21
+ */
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/src/";
+
+    public void run(final int port, final String url) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            socketChannel.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            socketChannel.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            socketChannel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            socketChannel.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind("192.168.10.127", port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        String url = DEFAULT_URL;
+        if (args.length > 1) {
+            url = args[1];
+        }
+        new HttpFileServer().run(port, url);
+    }
+}
